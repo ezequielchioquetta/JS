bed252c8de2d4a83731fb9af1785390c97df404c
EZEQUIEL SILVA CHIOQUETTA <126360689+tectiezequiel@users.noreply.github.com>
2025-06-05 19:29:54 -0300

    s

---
+++ aula06/exe008.js
@@ -0,0 +1,9 @@
+let vel = 66;
+
+console.log(`A velocidade do seu carro é ${vel} KM/h`);
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre com cinto de segurança`);
